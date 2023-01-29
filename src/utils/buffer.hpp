43d09623f7f9fc512778197e02548a423fe48364
h-hg <1394564544@qq.com>
2023-01-29 11:04:32 +0800

    add buffer

---
+++ src/utils/buffer.hpp
@@ -0,0 +1,166 @@
+#ifndef BUFFER_H_
+#define BUFFER_H_
+
+#include <algorithm>
+#include <memory>
+#include <string_view>
+
+class Buffer {
+ public:
+  /**
+   * @brief Construct the buffer.
+   * @param capacity the initial size of the Buffer
+   */
+  Buffer(size_t capacity)
+      : cap_(capacity),
+        data_(std::make_unique<char[]>(cap_)),
+        begin_ptr_(data_.get()),
+        read_ptr_(begin_ptr_),
+        write_ptr_(begin_ptr_) {}
+
+  ~Buffer() = default;
+
+  /**
+   * @brief Get the readable size
+   */
+  size_t readable_size() const { return write_ptr_ - read_ptr_; }
+  /**
+   * @brief Get the current read pointer
+   */
+  const char* cur_read_ptr() const { return read_ptr_; }
+  /**
+   * @brief Get the current read pointer
+   */
+  char* cur_read_ptr() { return read_ptr_; }
+
+  /**
+   * @brief Update the position of read pointer
+   * @note 需要自行判断是否会溢出缓冲区。
+   */
+  char* update_read_ptr(size_t step) { return read_ptr_ += step; }
+
+  /**
+   * @brief Get the view of buffer
+   */
+  std::string_view view() const { return {read_ptr_, write_ptr_}; }
+
+  /**
+   * @brief Get the writeable size
+   */
+  size_t writeable_size() const { (begin_ptr_ + cap_) - write_ptr_; }
+
+  /**
+   * @brief Get the current write pointer
+   */
+  const char* cur_write_ptr() const { return write_ptr_; }
+
+  /**
+   * @brief Get the current write pointer
+   */
+  char* cur_write_ptr() { return write_ptr_; }
+
+  /**
+   * @brief Get the readable size
+   */
+  size_t readable_size() const { return write_ptr_ - read_ptr_; }
+
+  /**
+   * @brief Get the current read pointer
+   */
+  const char* cur_read_ptr() const { return read_ptr_; }
+
+  /**
+   * @brief Get the current read pointer
+   */
+  char* cur_read_ptr() { return read_ptr_; }
+
+  /**
+   * @brief Make sure the Buffer can write data of the specified size
+   */
+  void ensuren_writeable(size_t size) {
+    if (writeable_size() >= size) return;
+    auto move_size = readable_size();
+    if ((read_ptr_ - begin_ptr_) + writeable_size() > size) {
+      // Move the position of the data to be readed.
+      std::copy(read_ptr_, write_ptr_, begin_ptr_);
+      // reset the pointer
+      read_ptr_ = begin_ptr_;
+      write_ptr_ = read_ptr_ + move_size;
+    } else {
+      // reallocate space
+      auto new_size = size * 2;
+      auto new_buf = std::make_unique<char[]>(new_size);
+      std::copy(read_ptr_, write_ptr_, new_buf.get());
+      // reset the pointer
+      data_ = std::move(new_buf);
+      cap_ = new_size;
+      begin_ptr_ = new_buf.get();
+      read_ptr_ = begin_ptr_;
+      write_ptr_ = read_ptr_ + move_size;
+    }
+  }
+
+  /**
+   * @brief Get data of the specified size from the Buffer
+   * @return Read size
+   */
+  ssize_t read(char* buffer, size_t size) {
+    auto read_size = std::min(size, readable_size());
+    // copy the data
+    std::copy(read_ptr_, read_ptr_ + read_size, buffer);
+    // update the pointer
+    read_ptr_ += read_size;
+    return read_size;
+  }
+
+  /**
+   * @brief Write data with the specified size to the Buffer
+   */
+  Buffer& write(const char* str, size_t size) {
+    ensuren_writeable(size);
+    // write the data
+    std::copy(str, str + size, write_ptr_);
+    // update write pointer
+    write_ptr_ += size;
+    return *this;
+  }
+
+  Buffer& write(std::string_view& str) { return write(str.data(), str.size()); }
+
+  Buffer& write(const Buffer& buffer) {
+    return write(buffer.cur_read_ptr(), buffer.readable_size());
+  }
+
+  /**
+   * @brief Reset the write pointer and read pointer
+   */
+  void clear() { write_ptr_ = read_ptr_ = begin_ptr_; }
+
+ protected:
+  /**
+   * @brief the memory to store the data.
+   */
+  std::unique_ptr<char[]> data_;
+
+  /**
+   * @brief Capacity, the size of data_.
+   */
+  size_t cap_;
+
+  /**
+   * @brief position of reader
+   */
+  char* read_ptr_;
+
+  /**
+   * @brief position of writer
+   */
+  char* write_ptr_;
+
+  /**
+   * @brief The starting position of buffer
+   */
+  char* begin_ptr_;
+};
+
+#endif
